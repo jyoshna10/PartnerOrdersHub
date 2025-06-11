3108bd5a920ca319c758dfed138efa6db62f52e3
jyoshna10 <jyoshna.kotakindam@gmai.com>
2025-06-11 12:42:04 -0500

    Initail Spring Boot project copied

---
+++ src/main/java/com/partner/PartnerOrdersHub/PartnerOrdersHubApplication.java
@@ -0,0 +1,13 @@
+package com.partner.PartnerOrdersHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerOrdersHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerOrdersHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partner/PartnerOrdersHub/PartnerOrdersHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partner.PartnerOrdersHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerOrdersHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
